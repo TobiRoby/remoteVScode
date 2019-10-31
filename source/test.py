4074a160bb99827b8333e251a0c23300456b4b85
Tobias Rippel <trippel@informationsfabrik.de>
2019-10-31 13:57:54 +0100

    extend test.py for pylama & mypy

--- source/test.py
+++ source/test.py
@@ -2,4 +2,20 @@ import os
 import pandas as pd
 
 os.getcwd()
-df = pd.DataFrame([1,2,3], columns=['col1'])
+
+
+def test_if_df(data: pd.DataFrame) -> bool:
+    """Test if passed data instance is from class DataFrame
+
+    Arguments:
+        data {pd.DataFrame} -- This is data input
+    Returns:
+        bool -- Returns bool if passed data is data
+    """
+
+    return(isinstance(df, pd.DataFrame))
+
+
+df = pd.DataFrame([1, 2, 3], columns=['col1'])\
+        .assign(long="looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong")
+assert(test_if_df(df))
