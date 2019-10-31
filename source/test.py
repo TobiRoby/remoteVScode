426dcbfad704928b4692e25e339e4fcc7c9b2cf1
Tobias Rippel <trippel@informationsfabrik.de>
2019-10-31 16:41:07 +0100

    implementing final pre-commit

--- source/test.py
+++ source/test.py
@@ -13,9 +13,11 @@ def test_if_df(data: pd.DataFrame) -> bool:
         bool -- Returns bool if passed data is data
     """
 
-    return (isinstance(df, pd.DataFrame))
+    return isinstance(df, pd.DataFrame)
 
 
-df = pd.DataFrame([1, 2, 3], columns=["col1"]).assign(long="looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong")
+df = pd.DataFrame([1, 2, 3], columns=["col1"]).assign(
+    long="looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong"
+)
 
 assert test_if_df(df)
